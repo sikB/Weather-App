e6694e18b3392205b81162c33031c4876c0bafa2
sikB <sikhovski@icloud.com>
2016-03-22 20:20:37 -0400

    Labels Hooked up to swift
    all the labels are now on swift

---
+++ WeatherApp/WeatherApp/MainViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MainViewController.swift
+//  WeatherApp
+//
+//  Created by Bogdan Sikhovski on 3/22/16.
+//  Copyright © 2016 Sik Marketing. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    @IBOutlet var dayNameLabel: UILabel!
+    
+    @IBOutlet var currTempLabel: UILabel!
+    
+    @IBOutlet var lowTempLabel: UILabel!
+    
+    @IBOutlet var weatherTypeLabel: UILabel!
+    
+    @IBOutlet var weatherImage: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+}
